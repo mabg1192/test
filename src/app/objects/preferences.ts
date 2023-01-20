0f56c16b4e92cc63ca3babe5d2ceddb8ff8fb10a
mabg1192 <mabg1192@proton.me>
2023-01-21 02:25:50 +0300

    Initial

---
+++ src/app/objects/preferences.ts
@@ -0,0 +1,19 @@
+export interface Prefs {
+
+    autoScrollFpl: boolean;
+
+    timeBeforeRest: number;
+    timeBetweenRest: number;
+    timeAfterRest: number;
+
+    departurePaTemplate: string;
+    arrivalPaTemplate: string;
+
+    tireLimitSpeed: number;
+
+    nCrewP: number;
+    myname: string;
+    fdCrew: string;
+    csdName: string;
+    csName: string; //Added CS name for Dep PA
+}
